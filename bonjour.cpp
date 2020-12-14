23b779ddc555110fa82bbeda97c263d228a8fbd0
emna guermazi <guermaziemna1@gmail.com>
2020-12-14 18:22:21 +0100

    j'ai ajouté deux variables

--- bonjour.cpp
+++ bonjour.cpp
@@ -1,8 +1,10 @@
+
 #include <iostream>
 #include <string>
 
 int main()
 {
+double a(0),b(0);//Déclaration de deux variables
   std::string name;
   std::cout << "Bonjour ";
  std::cout << "Modification 1 ";
