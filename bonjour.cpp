bddb6be4e562d0737035f9bb095b0f7f9f54a8f1
emnaeya <75843826+emnaeya@users.noreply.github.com>
2020-12-15 09:12:11 +0100

    Update bonjour.cpp
    afficher deux messages (donner une valeur pour a) et demander la valeur de a

--- bonjour.cpp
+++ bonjour.cpp
@@ -8,5 +8,8 @@ double a(0),b(0);//Déclaration de deux variables
   std::string name;
   std::cout << "Bonjour ";
  std::cout << "Modification 1 ";
+  cout << "Bienvenue dans le programme d'addition a+b !" << endl;
+     cout << "Donnez une valeur pour a : "; //On demande le premier nombre
+   cin >> a;
 
 }
